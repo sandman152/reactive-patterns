b066cb049ac4d95476517170c2c45b1b904980dc
Brian Sands <bsands@costar.com>
2018-10-25 14:02:59 -0400

    remove custom Observable implementation, use rxjs library instead

--- src/app/event-bus-experiments/app-data.ts
+++ src/app/event-bus-experiments/app-data.ts
@@ -1,58 +1,14 @@
-
+import { Subject, Observable, Observer } from 'rxjs';
+import { Lesson } from 'app/shared/model/lesson';
 import * as _ from 'lodash';
-import {Lesson} from "../shared/model/lesson";
-
-
-
-export interface Observer {
-    next(data:any);
-}
-
-export interface Observable {
-    subscribe(obs:Observer);
-    unsubscribe(obs:Observer);
-}
-
-
-interface Subject extends Observer, Observable {
-
-}
-
-
-class SubjectImplementation implements Subject {
-
-    private observers: Observer[] = [];
-
-    next(data: any) {
-        this.observers.forEach(obs => obs.next(data));
-    }
-
-    subscribe(obs: Observer) {
-        this.observers.push(obs);
-    }
-
-    unsubscribe(obs: Observer) {
-        _.remove(this.observers, el => el === obs);
-    }
-
-}
-
 
 class DataStore {
 
     private lessons : Lesson[]  = [];
 
-    private lessonsListSubject = new SubjectImplementation();
+    private lessonsListSubject = new Subject;
 
-    public lessonsList$: Observable = {
-
-        subscribe: obs => {
-            this.lessonsListSubject.subscribe(obs);
-            obs.next(this.lessons);
-        },
-
-        unsubscribe: obs => this.lessonsListSubject.unsubscribe(obs)
-    };
+    public lessonsList$: Observable<Lesson[]> = this.lessonsListSubject.asObservable();
 
     initializeLessonsList(newList: Lesson[]) {
         this.lessons = _.cloneDeep(newList);
